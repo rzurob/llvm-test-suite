281bf09914eb3542b0d8c3793bbd120450f8dc2a
Rafik Zurob <rzurob@ca.ibm.com>
2024-01-18 14:40:11 -0500

    Initial checkin of Fortran tests.

--- Fortran/IBM/move_nonrun_tests_to_compile.sh
+++
@@ -1,9 +0,0 @@
-for i in `cat lit.txt`
-do
-  filename=`basename $i`
-  filename_noext=`basename $filename .scenario`
-  pathname=${i%/$filename}
-
-  mkdir -p compile/$pathname
-  mv execute/$pathname/${filename_noext}.* compile/$pathname/
-done
--- Fortran/IBM/replace_zzrc_with_error_stop.sh
+++
@@ -1,5 +0,0 @@
-find . -name "*.[fF]*" -exec \
-	sed --in-place \
-	    -e '/CALL\>/ { s/CALL\s*zzrc\s*(\([0-9][0-9_]*\)\s*)/ERROR STOP \1/gi }' \
-	    -e 's/CALL\s*zzrc\s*(\([0-9][0-9_]*\)\s*)/error stop \1/gi' \
-	    {} \;
