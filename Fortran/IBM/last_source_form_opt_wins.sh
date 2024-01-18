edaa5335487000c89ecbfae9dba7af17e7427a8f
Rafik Zurob <rzurob@ca.ibm.com>
2024-01-18 14:40:11 -0500

    Initial checkin of Fortran tests.

--- Fortran/IBM/last_source_form_opt_wins.sh
+++
@@ -1,1 +0,0 @@
-find . -name "*.scenario*" -exec sed --in-place -f last_source_form_opt_wins.sedscript {} \;
--- Fortran/IBM/rename_vf_to_reference_output.sh
+++
@@ -1,4 +0,0 @@
-vf="$(realpath $1)"
-dir="$(dirname $vf)"
-base="$(basename $vf vf)"
-mv "$vf" "${dir}/${base}reference_output"
